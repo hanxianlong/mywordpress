328e276aca1bce2d1bf7e11955016abcf44cd342
hanxianlong <hanxianlong@vancl.com>
2013-02-26 18:05:16 +0800

    daily commit

--- wp-content/themes/epachina/header.php
+++ wp-content/themes/epachina/header.php
@@ -27,6 +27,7 @@ if ( !defined('ABSPATH')) exit;
  */ ?>
 <html>
 <head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta charset="<?php bloginfo('charset'); ?>" />
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0"/>
 <title><?php wp_title('&#124;', true, 'right'); ?></title>
